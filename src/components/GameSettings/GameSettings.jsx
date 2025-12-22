f186f774bb06372a3d5247b1c78bd5fa80ee920c
Mahdi <mahdiinhamburg@gmail.com>
2025-12-22 10:42:11 +0100

    style: change the style of the Game Settings /CLASSIC THEME

--- src/components/GameSettings/GameSettings.jsx
+++ src/components/GameSettings/GameSettings.jsx
@@ -2,14 +2,12 @@ import TypeBtn from "../../assets/Type_Btn.svg?react";
 import ThemeBtn from "../../assets/Theme_Btn.svg?react";
 import PieceBtn from "../../assets/Piece_Btn.svg?react";
 import VsFriendBtn from "../../assets/Vs_Friend_Btn.svg?react";
+import SettingsBtn from "../../assets/Settings_Btn.svg?react";
 
 export default function GameSettings() {
   return (
     <div className="gameSettingsContainer">
-      <TypeBtn className="gameSettingsBtn" />
-      <ThemeBtn className="gameSettingsBtn" />
-      <PieceBtn className="gameSettingsBtn" />
-      <VsFriendBtn className="gameSettingsBtn" />
+      <SettingsBtn className="settingsBtn" />
     </div>
   );
 }
