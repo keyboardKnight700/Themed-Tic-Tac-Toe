780176f9ef8233822eaed2db72f77fd1859d8eba
Mahdi <mahdiinhamburg@gmail.com>
2026-01-08 09:42:42 +0100

    feat: add the four settings buttons /CLASSIC THEME

--- src/components/GameSettings/GameSettings.jsx
+++ src/components/GameSettings/GameSettings.jsx
@@ -1,13 +1,20 @@
-import SettingsBtn from "../../assets/Settings_Btn.svg?react";
+import SettingsMainBtn from "../../assets/Settings_Btn.svg?react";
+import SettingsThemeBtn from "../../assets/SettingsBtnTheme.svg?react";
+import SettingsTypeBtn from "../../assets/SettingsBtnType.svg?react";
+import SettingsPieceBtn from "../../assets/SettingsBtnPiece.svg?react";
+import SettingsAiOrFriendBtn from "../../assets/SettingsBtnAiOrFriend.svg?react";
+
 import overlayCloseBtnBg from "../../assets/overlayCloseBtnBg.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function GameSettings() {
   const [overlay, setOverlay] = useState(false);
 
+  function handleClick() {}
+
   return (
     <div className="gameSettingsContainer">
-      <SettingsBtn
+      <SettingsMainBtn
         className="settingsMainBtn"
         onClick={() => setOverlay(true)}
       />
@@ -21,6 +28,15 @@ export default function GameSettings() {
               alt="x icon to close the window"
             />
           </button>
+
+          <div className="settingsContent"></div>
+
+          <div className="settingsBtns">
+            <SettingsThemeBtn className="settingBtn settingsThemeBtn" />
+            <SettingsTypeBtn className="settingBtn settingsTypeBtn" />
+            <SettingsPieceBtn className="settingBtn settingsPieceBtn" />
+            <SettingsAiOrFriendBtn className="settingBtn settingsAiOrFriendBtn" />
+          </div>
         </div>
       ) : (
         ""
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
+/* export default defineConfig({
   plugins: [react(), svgr()],
+});
+
+ */
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  // Disable cleanupIds so it doesn't clash with prefixIds
+                  cleanupIds: false,
+                },
+              },
+            },
+            // Enable prefixIds without params
+            // This forces it to use the filename (e.g., "SettingsBtnTheme_svg__a")
+            "prefixIds",
+          ],
+        },
+      },
+    }),
+  ],
 });
