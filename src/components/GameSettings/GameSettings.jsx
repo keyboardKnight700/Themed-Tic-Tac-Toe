066137a235266030f32d30724d89138c37dd398b
Mahdi <mahdiinhamburg@gmail.com>
2026-01-08 16:18:37 +0100

    refactor: refactor the four settings buttons /CLASSIC THEME

--- src/components/GameSettings/GameSettings.jsx
+++ src/components/GameSettings/GameSettings.jsx
@@ -1,46 +1,9 @@
-import SettingsMainBtn from "../../assets/Settings_Btn.svg?react";
-import SettingsThemeBtn from "../../assets/SettingsBtnTheme.svg?react";
-import SettingsTypeBtn from "../../assets/SettingsBtnType.svg?react";
-import SettingsPieceBtn from "../../assets/SettingsBtnPiece.svg?react";
-import SettingsAiOrFriendBtn from "../../assets/SettingsBtnAiOrFriend.svg?react";
-
-import overlayCloseBtnBg from "../../assets/overlayCloseBtnBg.svg";
-import { useState, useEffect } from "react";
+import { SettingsBtns } from "../UI/UI_Elements";
 
 export default function GameSettings() {
-  const [overlay, setOverlay] = useState(false);
-
-  function handleClick() {}
-
   return (
     <div className="gameSettingsContainer">
-      <SettingsMainBtn
-        className="settingsMainBtn"
-        onClick={() => setOverlay(true)}
-      />
-
-      {overlay ? (
-        <div className="settingsOverlay">
-          <button className="overlayCloseBtn" onClick={() => setOverlay(false)}>
-            <img
-              className="overlayCloseBtnBg"
-              src={overlayCloseBtnBg}
-              alt="x icon to close the window"
-            />
-          </button>
-
-          <div className="settingsContent"></div>
-
-          <div className="settingsBtns">
-            <SettingsThemeBtn className="settingBtn settingsThemeBtn" />
-            <SettingsTypeBtn className="settingBtn settingsTypeBtn" />
-            <SettingsPieceBtn className="settingBtn settingsPieceBtn" />
-            <SettingsAiOrFriendBtn className="settingBtn settingsAiOrFriendBtn" />
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+      <SettingsBtns />
     </div>
   );
 }
--- src/components/UI/UI_Elements.jsx
+++ src/components/UI/UI_Elements.jsx
@@ -1,5 +1,76 @@
-import settingsBtnType from "../../assets/settingsBtnType.svg";
+import SettingsMainBtn from "../../assets/Settings_Btn.svg?react";
+import SettingsThemeBtn from "../../assets/SettingsBtnTheme.svg?react";
+import SettingsTypeBtn from "../../assets/SettingsBtnType.svg?react";
+import SettingsPieceBtn from "../../assets/SettingsBtnPiece.svg?react";
+import SettingsAiOrFriendBtn from "../../assets/SettingsBtnAiOrFriend.svg?react";
+import overlayCloseBtnBg from "../../assets/overlayCloseBtnBg.svg";
 
-export function SettingsButton() {
-  return <button className="settingsBtn"></button>;
+import { useState, useEffect } from "react";
+
+export function SettingsBtns() {
+  const [overlay, setOverlay] = useState(false);
+  const [activeBtn, setActiveBtn] = useState("");
+  const Settings_Buttons = [
+    {
+      id: "type",
+      classes: "settingBtn settingsTypeBtn",
+      Component: SettingsTypeBtn,
+    },
+    {
+      id: "theme",
+      classes: "settingBtn settingsThemeBtn",
+      Component: SettingsThemeBtn,
+    },
+    {
+      id: "piece",
+      classes: "settingBtn settingsPieceBtn",
+      Component: SettingsPieceBtn,
+    },
+    {
+      id: "ai",
+      classes: "settingBtn settingsAiOrFriendBtn",
+      Component: SettingsAiOrFriendBtn,
+    },
+  ];
+
+  useEffect(() => {
+    if (overlay) {
+      setActiveBtn("type");
+    }
+  }, [overlay]);
+
+  return (
+    <>
+      <SettingsMainBtn
+        className="settingsMainBtn"
+        onClick={() => setOverlay(true)}
+      />
+
+      {overlay && (
+        <div className="settingsOverlay">
+          <button className="overlayCloseBtn" onClick={() => setOverlay(false)}>
+            <img
+              className="overlayCloseBtnBg"
+              src={overlayCloseBtnBg}
+              alt="x icon to close the window"
+            />
+          </button>
+
+          <div className="settingsContent"></div>
+
+          <div className="settingsBtnsContainer">
+            {Settings_Buttons.map(({ id, classes, Component }) => {
+              return (
+                <Component
+                  key={id}
+                  className={`${classes} ${activeBtn === id ? "active" : ""}`}
+                  onMouseEnter={() => setActiveBtn(id)}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
