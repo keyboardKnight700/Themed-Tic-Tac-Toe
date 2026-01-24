fae5acf32199bad6b4bb749f3720a6e0242d89b0
Mahdi <mahdiinhamburg@gmail.com>
2026-01-24 14:07:20 +0100

    Updates

--- src/components/GameSettings/GameSettings.jsx
+++
@@ -1,9 +0,0 @@
-import { SettingsPanel } from "../UI/UI_Elements";
-
-export default function GameSettings() {
-  return (
-    <div className="gameSettingsContainer">
-      <SettingsPanel />
-    </div>
-  );
-}
